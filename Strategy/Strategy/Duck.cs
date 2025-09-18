761186c512d937db83c831239f21829ff4c9df4a
tomptrs <tom_ptrs@yahoo.com>
2025-09-18 12:57:55 +0200

    initial setup

---
+++ Strategy/Strategy/Duck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    internal class Duck
+    {
+        public void quack()
+        {
+            Console.WriteLine("quack");
+        }
+
+        public void Swim()
+        {
+            Console.WriteLine("swim");
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("show duck");
+        }
+    }
+}
